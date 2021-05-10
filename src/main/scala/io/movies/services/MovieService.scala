5dc70df7589fcedf9f1845c549a9771dbbb3bdd5
Alozano <alberto.souto@47deg.com>
2021-05-10 16:13:01 +0200

    Add Service

---
+++ src/main/scala/io/movies/services/MovieService.scala
@@ -0,0 +1,26 @@
+package io.movies.services
+
+import io.movies.algebras.Repository
+import io.movies.model.{Movie, RegisteredMovie}
+
+trait MovieService[F[_]]{
+  def getMovieById(id: Short): F[Option[RegisteredMovie]]
+  def addMovie(movie: Movie): F[Int]
+}
+
+object MovieService{
+  def impl[F[_]](implicit repository: Repository[F]) =
+    new MovieService[F] {
+      override def getMovieById(id: Short): F[Option[RegisteredMovie]] = {
+        repository.getMovieById(id)
+      }
+
+      override def addMovie(movie: Movie): F[Int] = {
+        repository.addMovie(movie)
+      }
+    }
+}
+
+  // Con otra tabla de directores, se podría primero acceder a esa por nombre, coger id y usar
+  // ese id en la tabla movie
+
