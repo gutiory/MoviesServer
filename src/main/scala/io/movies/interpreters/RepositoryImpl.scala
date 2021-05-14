f9633eb9216d0b7d638e27446b60139f1d684dfc
Alozano <alberto.souto@47deg.com>
2021-05-14 16:20:13 +0200

    Fix use of Logger
    Only last expression was being executed, need to use flatMap or for-comprehension. As the second expression does not need the result of the first one we can use *>

--- src/main/scala/io/movies/interpreters/RepositoryImpl.scala
+++ src/main/scala/io/movies/interpreters/RepositoryImpl.scala
@@ -1,6 +1,7 @@
 package io.movies.interpreters
 
 import cats.effect._
+import cats.implicits._
 import doobie._
 import doobie.implicits._
 import io.movies.algebras.Repository
@@ -14,21 +15,18 @@ object RepositoryImpl {
     new Repository[F] {
     override def addMovie(movie: Movie): F[Int] = {
       val insert: Update0 = sql"insert into movie (title, director, year) values (${movie.title}, ${movie.director}, ${movie.releaseDate.getValue})".update
-      logger.info(s"add movie $movie")
-      insert.run.transact(transactor)
+      // Gets translated to expr1.flatMap(_ => expr2)
+      logger.info(s"add movie $movie") *> insert.run.transact(transactor)
     }
 
     override def getMovies: F[List[RegisteredMovie]] = {
       val select = sql"select id, title, director, year from movie".query[RegisteredMovie].to[List]
-      logger.info(s"get all movies with")
-      select.transact(transactor)
+      logger.info(s"get all movies") *> select.transact(transactor)
     }
 
     override def getMovieById(id: Int): F[Option[RegisteredMovie]] = {
       val select = sql"select id, title, director, year from movie where id = $id".query[RegisteredMovie].option
-      logger.info(s"get movie with id: $id")
-      logger.debug(s"get movie with id: $id")
-      select.transact(transactor)
+      logger.info(s"get movie with id: $id") *> select.transact(transactor)
     }
 
     override def createMovieTable: F[Int] = {
